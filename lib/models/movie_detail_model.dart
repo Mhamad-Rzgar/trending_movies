fcc350633cacedd33613496a8d4e9a2b4d96a1ba
Mhamad <54767803+Mhamad-Rzgar@users.noreply.github.com>
2024-07-20 13:10:10 +0300

    add MovieDetailModel and MovieModel classes
    - Implemented MovieDetailModel with overview, budget, revenue, and spokenLanguages fields
    - Added factory constructor for MovieDetailModel to parse JSON data
    - Created MovieModel with id, title, imageUrl, releaseDate, and optional detail fields
    - Included factory constructor for MovieModel to parse JSON data and utilize EndPoints for image URL

--- lib/models/movie_detail_model.dart
+++ lib/models/movie_detail_model.dart
@@ -1,1 +1,24 @@
+class MovieDetailModel {
+  final String overview;
+  final int? budget;
+  final int? revenue;
+  final List<String> spokenLanguages;
 
+  MovieDetailModel({
+    required this.overview,
+    this.budget,
+    this.revenue,
+    required this.spokenLanguages,
+  });
+
+  factory MovieDetailModel.fromJson(Map<String, dynamic> json) {
+    return MovieDetailModel(
+      overview: json['overview'],
+      budget: json['budget'],
+      revenue: json['revenue'],
+      spokenLanguages: json['spoken'] == null
+          ? <String>[]
+          : (json['spoken'] as List<String>).map((e) => e).toList(),
+    );
+  }
+}
--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -1,1 +1,32 @@
+import 'package:trending_movies/constants/end_points.dart';
 
+import 'movie_detail_model.dart';
+
+class MovieModel {
+  final int id;
+  final String title;
+  final String imageUrl;
+  final String releaseDate;
+  // Separate MovieDetailModel retrieved from a different API call to avoid numerous nullable fields.
+  final MovieDetailModel? detail;
+
+  MovieModel({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    required this.releaseDate,
+    this.detail,
+  });
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) {
+    return MovieModel(
+      id: json['id'],
+      title: json['title'],
+      imageUrl: EndPoints.imageBaseUrl + json['poster_path'],
+      releaseDate: json['release_date'] ?? 'Unknown',
+      detail: json['detail'] == null
+          ? null
+          : MovieDetailModel.fromJson(json['detail']),
+    );
+  }
+}
