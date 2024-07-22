cd5151df5124790ebdece66681c3d260e7f7bfd7
Mhamad <54767803+Mhamad-Rzgar@users.noreply.github.com>
2024-07-23 00:58:38 +0300

    docs: Add documentation for ConnectivityService
    - Documented ConnectivityService class and its hasInternetConnection method

--- lib/utils/connectivity_util.dart
+++ lib/utils/connectivity_util.dart
@@ -1,6 +1,11 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
+/// [ConnectivityService] is a service class that checks for internet connectivity.
 class ConnectivityService {
+  /// Checks if the device has an internet connection.
+  ///
+  /// Returns a [Future] containing a [bool] which is `true` if the device is
+  /// connected to the internet via mobile data or Wi-Fi, and `false` otherwise.
   Future<bool> hasInternetConnection() async {
     var connectivityResult = await (Connectivity().checkConnectivity());
     return connectivityResult == ConnectivityResult.mobile ||
